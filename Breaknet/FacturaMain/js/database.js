52004b5399a3b94b12fc0a4d633cfcbec269da0e
Fenixfgx <72283013+Fenixfgx@users.noreply.github.com>
2022-08-25 22:34:27 -0500

    Delete Breaknet directory

--- Breaknet/FacturaMain/js/database.js
+++
@@ -1,5 +0,0 @@
-[
-    {"claves":"David Zapata, 1069585617","description":"12 Mb","link":"https://fenixfgx.github.io/Break.net/Breaknet/Facturas/a1/a1.pdf","title":"David Zapata"},
-    
-    {"claves":"Angel Barragan, 1000000000","description":"15 Mb","link":"https://fenixfgx.github.io/Break.net/Breaknet/Facturas/a2/a1.pdf","title":"Angel Barragan"}
-]
